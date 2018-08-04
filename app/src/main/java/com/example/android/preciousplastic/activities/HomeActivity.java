c1d5144484331fe411df5e59e4203a5ecb74aab9
m-tantan <Yontymanor@gmail.com>
2018-08-04 20:56:46 +0300

    Attempting to make the drawer work.
    Still not working

--- app/src/main/java/com/example/android/preciousplastic/activities/HomeActivity.java
+++ app/src/main/java/com/example/android/preciousplastic/activities/HomeActivity.java
@@ -1,9 +1,20 @@
 package com.example.android.preciousplastic.activities;
 
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,7 +30,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 import org.osmdroid.views.MapView;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private final String TAG = "HOME_ACTIVITY";
 
@@ -115,6 +126,23 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         mUserRepository.updateUser(user);
     }
 
+    public void onOpenDrawer(View view)
+    {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
     public void onDecrementClick(View view) {
         User user = PPSession.currentUser();
         int pointTypeInt = Integer.parseInt(pointsTypeTextView.getText().toString());
@@ -138,8 +166,100 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case (R.id.button_decrement):
                 onDecrementClick(view);
                 break;
+            case (R.id.test_btn_drawer):
+//                onOpenDrawer(view);
+                break;
         }
     }
 
+    /**
+     * Called when an item in the navigation menu is selected.
+     *
+     * @param item The selected item
+     * @return true to display the item as the selected item
+     */
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id)
+        {
+
+            case R.id.drawer_home:
+            {
+                Toast.makeText(this, ("Clicked on " + "Home"), Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.drawer_workshops:
+            {
+                Toast.makeText(this, ("Clicked on " + "workshops"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_bazaar:
+            {
+                Toast.makeText(this, ("Clicked on " + "bazaar"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_map:
+            {
+                Toast.makeText(this, ("Clicked on " + "map"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_profile:
+            {
+                Toast.makeText(this, ("Clicked on " + "profile"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_my_workshop:
+            {
+                Toast.makeText(this, ("Clicked on " + "My workshop"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_settings:
+            {
+                Toast.makeText(this, ("Clicked on " + "settings"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_my_cart:
+            {
+                Toast.makeText(this, ("Clicked on " + "my cart"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_to_website:
+            {
+                Toast.makeText(this, ("Clicked on " + "website"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_about_us:
+            {
+                Toast.makeText(this, ("Clicked on " + "About us"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.drawer_sign_out:
+            {
+                Toast.makeText(this, ("Clicked on " + "Sign out"), Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            default:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
 
