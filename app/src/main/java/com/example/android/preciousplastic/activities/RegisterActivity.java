27fb4641beea33aec68636bb27d5b89b3db4a8f4
m-tantan <Yontymanor@gmail.com>
2018-09-26 14:45:45 +0300

    modified designs and edit texts, straigned lines etc'

--- app/src/main/java/com/example/android/preciousplastic/activities/RegisterActivity.java
+++ app/src/main/java/com/example/android/preciousplastic/activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.android.preciousplastic.activities;
 
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
